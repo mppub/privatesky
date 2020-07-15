638d7e7a2ea0617305f5ee6dcc58f9172c0fe813
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-07-15 15:55:11 +0300

    webshims env type setter refactor

--- builds/tmp/webshims.js
+++ builds/tmp/webshims.js
@@ -15,11 +15,13 @@ if (typeof(global.$$) == "undefined") {
 const or = require('overwrite-require');
 let allowedEnvTypes = [or.constants.BROWSER_ENVIRONMENT_TYPE, or.constants.SERVICE_WORKER_ENVIRONMENT_TYPE];
 
-if(allowedEnvTypes.indexOf($$.environmentType)!==-1){
-    console.log(`webshims already loaded in ${$$.environmentType}`)
-}
-else{
+//there are scenarios where webshims are loaded after other bundle (eg. swboot.js) and there is no need to set envType just check
+if(typeof $$.environmentType === "undefined"){
     or.enableForEnvironment(or.constants.BROWSER_ENVIRONMENT_TYPE);
+}else{
+    if(allowedEnvTypes.indexOf($$.environmentType)!==-1){
+        console.log(`Webshims bundle should be loaded only in ${JSON.stringify(allowedEnvTypes)}`);
+    }
 }
 
 if (typeof($$.__runtimeModules) == "undefined") {
