e56700d3e10aab404e5a4d4d7ba228aae30c48d7
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2020-07-16 10:28:29 +0300

    webshims display env type

--- builds/tmp/webshims.js
+++ builds/tmp/webshims.js
@@ -20,7 +20,7 @@ if(typeof $$.environmentType === "undefined"){
     or.enableForEnvironment(or.constants.BROWSER_ENVIRONMENT_TYPE);
 }else{
     if(allowedEnvTypes.indexOf($$.environmentType)!==-1){
-        console.log(`Webshims bundle should be loaded only in ${JSON.stringify(allowedEnvTypes)}`);
+        console.log(`Webshims bundle should be loaded only in ${JSON.stringify(allowedEnvTypes)}. Currently env type is: ${$$.environmentType}`);
     }
 }
 
