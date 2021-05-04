0c496a5db8b14a961206a5dd8e065af61991567e
skutner <sava.dumitru.daniel@gmail.com>
2021-05-04 16:34:46 +0300

    added test1 and test2 domains in BDNS

--- psknode/tests/util/tir.js
+++ psknode/tests/util/tir.js
@@ -373,6 +373,24 @@ const Tir = function () {
                     "anchoringServices": [
                         nodeUrl
                     ]
+                },
+                "test1": {
+                    "replicas": [],
+                    "brickStorages": [
+                        nodeUrl
+                    ],
+                    "anchoringServices": [
+                        nodeUrl
+                    ]
+                },
+                "test2": {
+                    "replicas": [],
+                    "brickStorages": [
+                        nodeUrl
+                    ],
+                    "anchoringServices": [
+                        nodeUrl
+                    ]
                 }
             }
 
@@ -758,17 +776,17 @@ const Tir = function () {
         );
 
         const pskBundlesPath = "./psknode/bundles";
-    
+
         const child_process = require("child_process");
         const forkedProcess = child_process.fork(scriptPath, [`--bundles=${pskBundlesPath}`, ...args], {
             cwd: process.env.PSK_ROOT_INSTALATION_FOLDER
         });
-    
+
         forkedProcess.on("exit", function (code) {
             if (code !== 0) {
                 return callback(code);
             }
-    
+
             callback(null);
         });
     }
