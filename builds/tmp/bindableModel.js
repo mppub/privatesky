7a32c217b2c7b1c0ad7442ea1ea5276e53233345
mastaleru <mastaleru.rafael@gmail.com>
2021-06-21 22:04:11 +0300

    fixes for node environment

--- builds/tmp/bindableModel.js
+++ builds/tmp/bindableModel.js
@@ -1,4 +1,9 @@
-if(typeof window.process === "undefined"){
+if (typeof window !== "undefined" && typeof window.process === "undefined") {
 	window.process = {};
 }
+
+if (typeof File === "undefined") {
+	global.File = function (){}
+}
+
 require("./bindableModel_intermediar");
